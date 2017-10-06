45afa174597fc370b1ed2c6c690826c8f428d9ca
Aralakh <servantofmanos@gmail.com>
2017-10-06 16:41:07 -0400

    Fixed an issue that caused the RecyclerView to scroll to the beginning of the photo list when subsequent pages were queried.

--- main/java/com/bignerdranch/android/photogallery/PhotoGalleryFragment.java
+++ main/java/com/bignerdranch/android/photogallery/PhotoGalleryFragment.java
@@ -35,17 +35,17 @@ public class PhotoGalleryFragment extends Fragment {
     private ThumbnailDownloader<PhotoHolder> mThumbnailDownloader;
 
     private boolean mIsLoading = true;
-    public int pastVisibleItem;
+    public int mPastVisibleItem;
     public int mMaxPage = 1;
     public int currentPage;
     public int mItemsPerPage;
 
-    public static PhotoGalleryFragment newInstance(){
+    public static PhotoGalleryFragment newInstance() {
         return new PhotoGalleryFragment();
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState){
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setRetainInstance(true);
         new FetchItemTask().execute();
@@ -65,13 +65,13 @@ public class PhotoGalleryFragment extends Fragment {
     }
 
     @Override
-    public void onDestroyView(){
+    public void onDestroyView() {
         super.onDestroyView();
         mThumbnailDownloader.clearQueue();
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_photo_gallery, container, false);
 
         mPhotoRecyclerView = (RecyclerView) v.findViewById(R.id.photo_recycler_view);
@@ -79,14 +79,12 @@ public class PhotoGalleryFragment extends Fragment {
         mPhotoRecyclerView.setLayoutManager(mGridLayoutManager);
 
 
-
-        mPhotoRecyclerView.addOnScrollListener(new RecyclerView.OnScrollListener(){
+        mPhotoRecyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
             @Override
             public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
                 super.onScrolled(recyclerView, dx, dy);
-                pastVisibleItem = mGridLayoutManager.findLastVisibleItemPosition();
-
-                if(!mIsLoading && (pastVisibleItem >= mItems.size()-1) && (currentPage < mMaxPage)){
+                mPastVisibleItem = mGridLayoutManager.findLastVisibleItemPosition();
+                if (!mIsLoading && (mPastVisibleItem >= mItems.size() - 1) && (currentPage < mMaxPage)) {
                     mIsLoading = true;
                     currentPage++;
                     new FetchItemTask().execute();
@@ -95,48 +93,51 @@ public class PhotoGalleryFragment extends Fragment {
 
         });
 
-        setupAdapter();
-
         return v;
     }
 
     @Override
-    public void onDestroy(){
+    public void onDestroy() {
         super.onDestroy();
         mThumbnailDownloader.quit();
         Log.i(TAG, "Background thread destroyed.");
     }
 
+    private void setupAdapter() {
+        if (isAdded()) {
+            mPhotoRecyclerView.setAdapter(new PhotoAdapter(mItems));
+        }
+    }
 
-    private class PhotoHolder extends RecyclerView.ViewHolder{
+    private class PhotoHolder extends RecyclerView.ViewHolder {
         private ImageView mItemImageView;
 
-        public PhotoHolder(View itemView){
+        public PhotoHolder(View itemView) {
             super(itemView);
             mItemImageView = (ImageView) itemView;
         }
 
-        public void bindDrawable(Drawable drawable){
+        public void bindDrawable(Drawable drawable) {
             mItemImageView.setImageDrawable(drawable);
         }
     }
 
-    private class PhotoAdapter extends RecyclerView.Adapter<PhotoHolder>{
+    private class PhotoAdapter extends RecyclerView.Adapter<PhotoHolder> {
         private List<GalleryItem> mGalleryItems;
 
-        public PhotoAdapter(List<GalleryItem> galleryItems){
+        public PhotoAdapter(List<GalleryItem> galleryItems) {
             mGalleryItems = galleryItems;
         }
 
         @Override
-        public PhotoHolder onCreateViewHolder(ViewGroup viewGroup, int viewType){
+        public PhotoHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
             LayoutInflater inflater = LayoutInflater.from(getActivity());
             View view = inflater.inflate(R.layout.list_item_gallery, viewGroup, false);
-            return  new PhotoHolder(view);
+            return new PhotoHolder(view);
         }
 
         @Override
-        public void onBindViewHolder(PhotoHolder photoHolder, int position){
+        public void onBindViewHolder(PhotoHolder photoHolder, int position) {
             GalleryItem galleryItem = mGalleryItems.get(position);
             Drawable placeholder = ContextCompat.getDrawable(getActivity(), R.drawable.blaze);
             photoHolder.bindDrawable(placeholder);
@@ -144,64 +145,48 @@ public class PhotoGalleryFragment extends Fragment {
         }
 
         @Override
-        public int getItemCount(){
+        public int getItemCount() {
             return mGalleryItems.size();
         }
     }
 
-    private class FetchItemTask extends AsyncTask<Void, Void, List<GalleryItem>>{
+    private class FetchItemTask extends AsyncTask<Void, Void, List<GalleryItem>> {
         @Override
-        protected List<GalleryItem> doInBackground(Void... params){
+        protected List<GalleryItem> doInBackground(Void... params) {
             return mFlickrFetcher.fetchItems(currentPage);
         }
 
         @Override
-        protected void onPostExecute(List<GalleryItem> items){
+        protected void onPostExecute(List<GalleryItem> items) {
             //first time querying data
-            if(mItems.size() == 0){
+            if (mItems.size() == 0) {
                 mMaxPage = mFlickrFetcher.getMaxPages();
                 mItemsPerPage = mFlickrFetcher.getItemsPerPage();
                 mItems.addAll(items);
 
-                mPhotoRecyclerView.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener(){
+                mPhotoRecyclerView.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
                     @Override
                     //dynamically scale number of columns based on device size
-                    public void onGlobalLayout(){
-                        mPhotoRecyclerView.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+                    public void onGlobalLayout() {
                         int width = mPhotoRecyclerView.getMeasuredWidth();
-                        float columnWidth = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 150, getActivity().getResources().getDisplayMetrics());
-                        int columnNumber = Math.round(width/columnWidth);
+                        float columnWidth = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 120, getActivity().getResources().getDisplayMetrics());
+                        int columnNumber = Math.round(width / columnWidth);
                         mGridLayoutManager = new GridLayoutManager(getActivity(), columnNumber);
                         mPhotoRecyclerView.setLayoutManager(mGridLayoutManager);
-                        setupAdapter();
+                        mPhotoRecyclerView.getViewTreeObserver().removeOnGlobalLayoutListener(this);
                     }
                 });
-            }else{
+
+                setupAdapter();
+            } else {
                 final int prevSize = mItems.size();
                 mItems.addAll(items);
-                mPhotoRecyclerView.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener(){
-                    @Override
-                    //dynamically scale number of columns based on device size
-                    public void onGlobalLayout(){
-                        mPhotoRecyclerView.getViewTreeObserver().removeOnGlobalLayoutListener(this);
-                        int width = mPhotoRecyclerView.getMeasuredWidth();
-                        float columnWidth = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 150, getActivity().getResources().getDisplayMetrics());
-                        int columnNumber = Math.round(width/columnWidth);
-                        mGridLayoutManager = new GridLayoutManager(getActivity(), columnNumber);
-                        mPhotoRecyclerView.setLayoutManager(mGridLayoutManager);
-                        mPhotoRecyclerView.smoothScrollToPosition(prevSize);
-                        setupAdapter();
-                    }
-                });
+
+                mPhotoRecyclerView.getAdapter().notifyItemRangeInserted(prevSize, mItemsPerPage);
+                mPhotoRecyclerView.smoothScrollToPosition(prevSize);
+
             }
             mIsLoading = false;
-            mPhotoRecyclerView.getAdapter().notifyDataSetChanged();
-        }
-    }
-
-    private void setupAdapter(){
-        if(isAdded()){
-            mPhotoRecyclerView.setAdapter(new PhotoAdapter(mItems));
         }
     }
 }
